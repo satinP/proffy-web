685741e61c08d59ea4eb07a3a2c5295437fe763f
Pedro Satin <pedro5satin@gmail.com>
2020-08-03 21:22:03 -0300

    Landing page mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Landing from './pages/Landing'
 import './assets/styles/global.css';
 
 function App() {
   return (
-    <p>Hello World!</p>
+    <Landing />
   );
 }
 
