1126d2361f2e8ed32a77984e9c6cba6d11f4c76e
Pedro Satin <pedro5satin@gmail.com>
2020-08-06 21:15:41 -0300

    Finalizando TeacherList

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -3,36 +3,52 @@ import React from 'react';
 import './styles.css';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({teacher}) => {
+
+  function createNewConnection() {
+    api.post('/connections', {user_id : teacher.id});
+  }
+
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://p2.trrsf.com/image/fget/cf/460/0/images.terra.com/2013/03/14/einstein-arquivo-afp.jpg" alt="Albert Einstein"/>
+        <img src={teacher.avatar} alt={teacher.name}/>
         
         <div>
-          <strong>Albert Einstein</strong>
-          <span>Física</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
 
       </header>
       
-      <p>
-        Físico teórico alemão que desenvolveu a teoria da relatividade geral.
-        <br /> <br />
-        Laureado com o Prêmio Nobel de Física de 1921.
-      </p>
+      <p> {teacher.bio} </p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>U$ 3.000</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
 
-        <button type="button">
+        <a href={`https://wa.me/${teacher.whatsapp}`} target="_blank"
+          onClick={createNewConnection}>
           <img src={whatsappIcon} alt="Whatsapp"/>
           Entrar em contato
-        </button>
+        </a>
         
       </footer>
     </article>
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,50 +1,79 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 
 import './styles.css';
 
 import PageHeader from '../../components/PageHeader';
-import TeacherItem from '../../components/TeacherItem';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
 import Input from '../../components/Input';
 import Select from '../../components/Select';
+import api from '../../services/api';
 
 
 function TeacherList () {
+
+  const [teachers, setTeachers] = useState([]);
+
+  const [subject, setSubject] = useState('');
+  const [week_day, setWeekDay] = useState('');
+  const [time, setTime] = useState('');
+
+  function searchTeachers(e: FormEvent) {
+    e.preventDefault();
+
+    api.get('/classes', {
+      params: {
+        subject,
+        week_day,
+        time
+      }
+    }).then(res => {
+      setTeachers(res.data);
+    }).catch( e => {
+      console.log('erro');
+    });
+  }
+
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Esses são os proffys disponíveis." >
-        <form id="search-teachers">
+        <form id="search-teachers" onSubmit={searchTeachers}>
 
-          <Select name="subject" label="Matéria" options={[
-            {value: 'Artes', label: 'Artes'},
-            {value: 'Biologia', label: 'Biologia'},
-            {value: 'Física', label: 'Física'},
-            {value: 'Matemática', label: 'Matemática'},
-            {value: 'Filosofia', label: 'Filosofia'},
-            {value: 'Português', label: 'Português'},
-            {value: 'Química', label: 'Química'},
-            {value: 'Sociologia', label: 'Sociologia'},
-          ]} />
+          <Select name="subject" label="Matéria" value={subject}
+            onChange={(e) => { setSubject(e.target.value); }}
+            options={[
+              {value: 'Artes', label: 'Artes'},
+              {value: 'Biologia', label: 'Biologia'},
+              {value: 'Física', label: 'Física'},
+              {value: 'Matemática', label: 'Matemática'},
+              {value: 'Filosofia', label: 'Filosofia'},
+              {value: 'Português', label: 'Português'},
+              {value: 'Química', label: 'Química'},
+              {value: 'Sociologia', label: 'Sociologia'},
+            ]} />
 
-          <Select name="week_day" label="Dia da Semana" options={[
-            {value: '0', label: 'Domingo'},
-            {value: '1', label: 'Segunda-feira'},
-            {value: '2', label: 'Terça-feira'},
-            {value: '3', label: 'Quarta-feira'},
-            {value: '4', label: 'Quinta-feira'},
-            {value: '5', label: 'Sexta-feira'},
-            {value: '6', label: 'Sábado'},
-          ]} />
+          <Select name="week_day" label="Dia da Semana" value={week_day}
+            onChange={(e) => { setWeekDay(e.target.value); }}
+            options={[
+              {value: '0', label: 'Domingo'},
+              {value: '1', label: 'Segunda-feira'},
+              {value: '2', label: 'Terça-feira'},
+              {value: '3', label: 'Quarta-feira'},
+              {value: '4', label: 'Quinta-feira'},
+              {value: '5', label: 'Sexta-feira'},
+              {value: '6', label: 'Sábado'},
+            ]} />
 
-          <Input type="time" name="time" label="Hora"/>
+          <Input type="time" name="time" label="Hora" value={time}
+            onChange={(e) => { setTime(e.target.value); }} />
 
+          <button type="submit">Buscar</button>
         </form>
       </PageHeader>
 
       <main>
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
+        {teachers.map((teacher:Teacher) => {
+          return <TeacherItem key={teacher.id} teacher={teacher} />
+        })}
       </main>
 
     </div>
